c8380cd0a6329b78c19e2428f851d22bd6772e05
Fayaz <124581240+mdfayaz82@users.noreply.github.com>
2025-03-04 22:49:37 +0400

    Add files via upload

---
+++ RequestDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using RequestSample.Models;
+
+namespace RequestSample
+{
+    public class RequestDBContext : DbContext
+    {
+        public DbSet<Request> Requests { get; set; }
+    }
+}
